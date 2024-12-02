9dff99bb9df6198478d851a1d434ffeb54efd33e
Senesh Akmeemana <seneshak2001@gmail.com>
2024-12-03 02:08:16 +0530

    build: Configured datebase and created app named 'emp_management'.

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employee_management',
+        'USER': 'postgres',
+        'PASSWORD': 'senesh@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
